4f35f04397220857bd96ff07e5323ef340842d85
Ilya <sobin_98@mail.ru>
2022-08-07 17:57:38 +0400

    finally edding

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -14,19 +14,60 @@ class ViewController: UIViewController {
     @IBOutlet var valueGreenSlider: UILabel!
     @IBOutlet var valueBlueSlider: UILabel!
     
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        setupSliders()
+        setupLable()
     }
 
-    @IBAction func redSlider(_ sender: Any) {
+    @IBAction func redSliderAction() {
+        valueRedSlider.text = String(round(redSlider.value * 1000) / 1000)
+        setupLable()
+    }
+    @IBAction func greenSliderAction() {
+        valueGreenSlider.text = String(round(greenSlider.value * 1000) / 1000)
+        setupLable()
+    }
+    @IBAction func blueSliderAction() {
+        valueBlueSlider.text = String(round(blueSlider.value * 1000) / 1000)
+        setupLable()
     }
     
-    @IBAction func greenSlider(_ sender: Any) {
+    // MARK: - Private Methods
+    private func setupLable() {
+        colorizedView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
     
-    @IBAction func blueSlider(_ sender: Any) {
+    private func setupSliders() {
+        setupSluderRed()
+        setupSliderGreen()
+        setupSliderBleu()
     }
     
+    private func setupSluderRed() {
+        redSlider.value = 0.5
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        redSlider.minimumTrackTintColor = .red
+        
+    }
+    private func setupSliderGreen() {
+        greenSlider.value = 0.5
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        greenSlider.minimumTrackTintColor = .green
+        
+    }
+    private func setupSliderBleu() {
+        blueSlider.value = 0.5
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        blueSlider.minimumTrackTintColor = .blue
+        
+    }
 }
 
