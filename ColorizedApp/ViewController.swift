a9900d8e6b2f358a1241db57b3fa6d5ac44b3dd3
Ilya <sobin_98@mail.ru>
2022-08-07 20:43:33 +0400

    La Finally

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var colorizedView: UIView!
+    @IBOutlet var opacitySlider: UISlider!
     
     @IBOutlet var valueRedSlider: UILabel!
     @IBOutlet var valueGreenSlider: UILabel!
@@ -18,12 +19,21 @@ class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
+    @IBOutlet var saveColor: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         setupSliders()
-        setupLable()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        bottomSetting()
     }
 
+    @IBAction func changeOpacitySlider() {
+        setupLable()
+    }
     @IBAction func redSliderAction() {
         valueRedSlider.text = String(round(redSlider.value * 1000) / 1000)
         setupLable()
@@ -37,15 +47,27 @@ class ViewController: UIViewController {
         setupLable()
     }
     
+    @IBAction func saveColorView() {
+        view.backgroundColor = colorizedView.backgroundColor
+    }
+    
     // MARK: - Private Methods
     private func setupLable() {
-        colorizedView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        colorizedView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(opacitySlider.value))
     }
     
     private func setupSliders() {
+        setupSluderOpacity()
         setupSluderRed()
         setupSliderGreen()
         setupSliderBleu()
+    }
+    
+    private func setupSluderOpacity() {
+        opacitySlider.value = 0.5
+        opacitySlider.minimumValue = 0
+        opacitySlider.maximumValue = 1
+        
     }
     
     private func setupSluderRed() {
@@ -53,21 +75,26 @@ class ViewController: UIViewController {
         redSlider.minimumValue = 0
         redSlider.maximumValue = 1
         redSlider.minimumTrackTintColor = .red
-        
+        valueRedSlider.text = String(redSlider.value)
     }
     private func setupSliderGreen() {
         greenSlider.value = 0.5
         greenSlider.minimumValue = 0
         greenSlider.maximumValue = 1
         greenSlider.minimumTrackTintColor = .green
-        
+        valueGreenSlider.text = String(greenSlider.value)
     }
     private func setupSliderBleu() {
         blueSlider.value = 0.5
         blueSlider.minimumValue = 0
         blueSlider.maximumValue = 1
         blueSlider.minimumTrackTintColor = .blue
-        
+        valueBlueSlider.text = String(blueSlider.value)
+    }
+    
+    private func bottomSetting() {
+        saveColor.backgroundColor = .white
+        saveColor.layer.cornerRadius = 20
     }
 }
 
